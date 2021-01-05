561f66fa316629d569f21b0f006a08be0f0f268e
yuseon <s20036@gsm.hs.kr>
2021-01-06 00:34:42 +0900

    gsm calender 6

--- new_calender.js
+++ new_calender.js
@@ -3,12 +3,17 @@
 let date = new Date();
 
 const renderCalendar = () => {
-    const viewYear = date.getFullYear();    
+
+    const viewYear = date.getFullYear();  
+    //원하는 값 넣기  
     const viewMonth= new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" );
 
+    // 배열[달] = 원하는 값
     var yearMonth = document.querySelector('.year-month').textContent = (viewMonth[date.getMonth()]+" "+date.getFullYear());
 
+    // 지난 달의 마지막 날을 구함 (다음 달 처음을 만들기 위함) 
     const prevLast = new Date(date.getFullYear(), date.getMonth(), 0);
+    // 이번 달의 마지막 날을 구함
     const thisLast = new Date(date.getFullYear(), date.getMonth() + 1, 0);
 
     const PLDate = prevLast.getDate();
@@ -16,6 +21,8 @@ const renderCalendar = () => {
 
     const TLDate = thisLast.getDate();
     const TLDay = thisLast.getDay();
+
+    // Dates 기본 배열들
 
     const prevDates = [];
     const thisDates = [...Array(TLDate + 1).keys()].slice(1);
@@ -34,11 +41,30 @@ const renderCalendar = () => {
 
     const dates = prevDates.concat(thisDates, nextDates);
 
+    const firstDateIndex = dates.indexOf(1);
+    const lastDateIndex = dates.lastIndexOf(TLDate);
     dates.forEach((date, i) => {
-    dates[i] = `<div class="date">${date}</div>`;
+        const condition = i >= firstDateIndex && i < lastDateIndex + 1
+                        ? 'this'
+                        : 'other';
+
+        dates[i] = `<div class="date"><span class="${condition}">${date}</span></div>`;
     })
 
+    
+    // 데이터 그리기
     document.querySelector('.dates').innerHTML = dates.join('');
+    
+    //오늘 날짜 설정
+    const today = new Date();
+    if (viewMonth == today.getMonth && viewYear == today.getFullYear) {
+      for (let date of document.querySelectorAll('this')) {
+        if (+date.innerText == today.getDate()) {
+          date.classList.add('today');
+          break;
+        }
+      }
+    }
 
   }
   
@@ -57,25 +83,4 @@ const nextMonth = () => {
 const goToday = () => {
     date = new Date();
     renderCalendar();
-}
-
-// Dates 정리
-const firstDateIndex = dates.indexOf(1);
-const lastDateIndex = dates.lastIndexOf(TLDate);
-dates.forEach((date, i) => {
-const condition = i >= firstDateIndex && i < lastDateIndex + 1
-                    ? 'this'
-                    : 'other';
-
-dates[i] = `<div class="date"><span class="${condition}">${date}</span></div>`;
-})
-
-const today = new Date();
-  if (viewMonth === today.getMonth && viewYear === today.getFullYear) {
-    for (let date of document.querySelectorAll('this')) {
-      if (+date.innerText === today.getDate()) {
-        date.classList.add('today');
-        break;
-      }
-    }
 }
